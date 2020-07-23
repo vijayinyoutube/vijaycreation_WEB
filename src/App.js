68434f0b4f2c4ad7ece2cc0873cdf27a3ecf061d
Vijay <vijaythevirtue@gmail.com>
2020-07-23 17:26:25 +0530

    web app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Navbar from "./components/NavBar";
 import Welcome from "./components/Welcome";
 import AOI from "./components/AOI";
+import Feature1 from "./components/Feature1";
+import Feature2 from "./components/Feature2";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Navbar></Navbar>
       <Welcome></Welcome>
       <AOI></AOI>
+      <Feature1></Feature1>
+      <Feature2></Feature2>
     </div>
   );
 }
